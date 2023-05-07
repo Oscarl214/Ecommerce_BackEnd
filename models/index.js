9a3b73be66c19d7d2f4d6208fbf53049ba52720e
Oscar Leal <oscarleal234@gmail.com>
2023-05-06 23:25:12 -0500

    new changes

---
+++ models/index.js
@@ -0,0 +1,43 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+
+//Product
+Product.belongsTo(Category, {
+  foreignkey: 'category_id',
+})
+Product.belongsToMany(Tag,{
+  through: ProductTag,
+  foreignKey: 'product_id'
+})
+
+//Category
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+})
+
+//Tag
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignkey: 'tag_id',
+})
+
+//ProductTag
+
+ProductTag.belongsTo(Product, {
+  foreignKey: 'product_id'
+});
+ProductTag.belongsTo(Tag, {
+  foreignKey: 'tag_id'
+});
+
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
